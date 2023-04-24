500ab28f4d7d282e71cc9c90bc84ec75cc5c7edb
RatedA <orri@andreassen.is>
2023-04-24 22:36:02 +0000

    now with working json dumping

--- streymendur.py
+++ streymendur.py
@@ -12,6 +12,7 @@ import datetime
 
 """
 #https://pytwitchapi.readthedocs.io/en/stable/index.html
+ 
 with open('./config.toml', 'rb') as f:
     config = tomllib.load(f)
 
@@ -24,29 +25,27 @@ async def get_streams():
     twitch = await Twitch(app_id, app_secret) 
     async for stream in twitch.get_streams(stream_type='live'):
         #yielda stream sem async stream
-            yield stream
+        yield stream
         
+
 async def write_to_json():
     start_time = time.perf_counter()
     streams = get_streams()
     searchterm = ['icelandic', 'iceland', 'Icelandic', 'Iceland']
     timecheck = datetime.datetime.now()
     formattime = timecheck.strftime("%d-%m-%Y_%H-%M")
+    dumpdict = {}
     #filtering magic
-    with open('./Streymendur/'+'twitch_'+formattime+'.json', mode='w', encoding="utf-8", errors='ignore') as file:
+    with open('./data/'+'twitch_'+formattime+'.json', mode='w', encoding="utf-8", errors='ignore') as file:
         async for stream in streams:
             #from async stream to dict to json + filtering
             if stream.tags is not None and any(term in stream.tags for term in searchterm):
-                jsonread = {
-                 stream.id: {
-                 'user_name':stream.user_login,
-                 'viewers':stream.viewer_count,
-                 'game':stream.game_name,
-                 'tags':stream.tags
-                 }
-            }
-                writer = json.dumps(jsonread, indent=4, ensure_ascii=False)
-                file.write(writer)
+                jsonread = {stream.user_login:{'stream_id':stream.user_id}}
+                print(jsonread)
+                dumpdict.update(jsonread)
+                
+        writer = json.dumps(dumpdict, ensure_ascii=False, indent=4, sort_keys=True,)
+        file.write(writer)
 
         
     end_time = time.perf_counter()
